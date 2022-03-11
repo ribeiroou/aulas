5e90ecec5e5ee4d4c0aaac1284f5382584320005
ribeiroo <93227849+ribeiroou@users.noreply.github.com>
2022-03-11 10:11:49 -0300

    Update .ino

--- exercicio5/.ino
+++ exercicio5/.ino
@@ -1,1 +1,25 @@
+const int botao1 = 7; 
+const int led1 = 8; 
 
+int var1;
+
+void setup()
+{
+  pinMode(botao1, INPUT);
+  pinMode(led1, OUTPUT);
+  //Serial.begin(9600);
+}
+
+void loop()
+{
+  
+  var1 = digitalRead(botao1);
+  
+  if(var1){
+  digitalWrite(led1,LOW);
+  }else{ 
+    digitalWrite(led1, HIGH);
+  
+  } 
+  delay(10);
+}
